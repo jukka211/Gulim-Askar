98165c60fde5d447ef07c7c28613727410df9176
jukka211 <158295066+jukka211@users.noreply.github.com>
2024-08-14 14:33:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const displayDistance = 100; // distance in px to display another photo
-const nDisplay = 20; // number of pictures to display at once
+let nDisplay = 6; // default number of pictures to display at once
+
+// Check if the screen width is 768px or less
+if (window.innerWidth <= 768) {
+    nDisplay = 4; // number of pictures to display at once on mobile
+}
 
 const images = document.getElementsByClassName("image");
 
@@ -10,7 +15,7 @@ let lastMousePosition = {x: 0, y: 0}; // used to get the last mouse/touch positi
 function activatePic(img, x, y) {
     // Reset the scale of all images
     for (let i = 0; i < images.length; i++) {
-        images[i].style.transform = "translate(-50%, -50%) scale(0.6)";
+        images[i].style.transform = "translate(-50%, -50%) scale(0.5)";
     }
 
     // Activate the current image and scale it up
